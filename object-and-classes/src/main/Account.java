c28166872f3207cc8d4b4efcdca2f460f391af4c
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-04 22:19:59 +0000

    changed folder

---
+++ object-and-classes/src/main/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    private int totalTrades;
+
+    public int getTotalTrades() {
+        return totalTrades;
+    }
+
+    public void setTotalTrades(int totalTrades) {
+        this.totalTrades = totalTrades;
+    }
+}
